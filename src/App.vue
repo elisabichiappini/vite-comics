4675f79730d0c27501e015f8a968d9b2656b639f
elisabichiappini <elisabichiappini@gmail.com>
2023-12-09 17:43:43 +0100

    feat: scaffolding project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script> </script>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
-<template> </template>
+export default {
+    name: 'App',
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter,
+    }
+};
+</script>
 
-<style scoped> </style>
+<template>
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+    <AppFooter></AppFooter>
+</template>
+
+<style lang="scss"> </style>
