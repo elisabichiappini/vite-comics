6389e80c7d97c8d85095843a8ea5748066a7a7b5
elisabichiappini <elisabichiappini@gmail.com>
2023-12-10 20:48:27 +0100

    feat: dinamic main list

--- src/components/main/AppMain.vue
+++ src/components/main/AppMain.vue
@@ -1,6 +1,37 @@
 <script>
 export default {
     name: 'AppMain',
+    data () {
+        return {
+            listQualities: [
+                {
+                    nome: 'Digital comics',
+                    src: '../../../public/img/buy-comics-digital-comics.png',
+                    alt: 'digital-comics-image'
+                },
+                {
+                    nome: 'Dc merchandise',
+                    src: '../../../public/img/buy-comics-merchandise.png',
+                    alt: 'comics-merchandise'
+                },
+                {
+                    nome: 'Subscription',
+                    src: '../../../public/img/buy-comics-subscriptions.png',
+                    alt: 'comics-subscriptions'
+                },
+                {
+                    nome: 'Comics shop locator',
+                    src: '../../../public/img/buy-comics-shop-locator.png',
+                    alt: 'shop-locator'
+                },
+                {
+                    nome: 'Dc power visa',
+                    src: '../../../public/img/buy-dc-power-visa.svg',
+                    alt: 'power-visa'
+                },
+            ]
+        }
+    }
 };
 </script>
 
@@ -15,38 +46,23 @@ export default {
     <div id="main-bottom">
         <div class="container">
             <ul class="list-skills">
-                <li class="skill-content">
-                    <img src="../../../public/img/buy-comics-digital-comics.png" alt="digitalComics">
-                    <h3>Digital Comics</h3>
-                </li>
-                <li class="skill-content">
-                    <img src="../../../public/img/buy-comics-merchandise.png" alt="digitalComics">
-                    <h3>Digital Comics</h3>
-                </li>
-                <li class="skill-content">
-                    <img src="../../../public/img/buy-comics-shop-locator.png" alt="digitalComics">
-                    <h3>Digital Comics</h3>
-                </li>
-                <li class="skill-content">
-                    <img src="../../../public/img/buy-comics-subscriptions.png" alt="digitalComics">
-                    <h3>Digital Comics</h3>
-                </li>
-                <li class="skill-content">
-                    <img src="../../../public/img/buy-comics-merchandise.png" alt="digitalComics">
-                    <h3>Digital Comics</h3>
+                <li class="skill-content" v-for="li in listQualities">
+                    <img :src="li.src" :alt="li.alt">
+                    <h3>{{ li.nome }}</h3>
                 </li>
             </ul>
         </div>
     </div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 @use '../../styles/partials/variables' as *;
 @use '../../styles/general.scss' as *;
 @use '../../styles/partials/mixin.scss' as *;
 
 #main-top {            
     background-color: $bg-main;
+
     .container {
         @include my-container-size;
 
@@ -60,25 +76,27 @@ export default {
 #main-bottom {            
     background-color: $bg-primary;
     color: $color-white;
+
     .container {
         @include my-container-size;
         padding: 50px 0;
+
         .list-skills {
             @include my-flex;
             justify-content: center;
             gap: 30px;
-            .skill-content{
+
+            .skill-content {
                 @include my-flex;
+                @include my-uppercase;
                 justify-content: center;
                 gap: 15px;
-                text-transform: uppercase;
 
                 img {
                     width: 50px;
                 }
             }
         }
-    
     }
 }
 </style>
